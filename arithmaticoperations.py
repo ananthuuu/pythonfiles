4e85645739c94471e9aee83f6789f562a823983e
ananthuuu <ananthcjayan@gmail.com>
2022-09-02 21:38:02 +0530

    secondcommit

---
+++ arithmaticoperations.py
@@ -0,0 +1,18 @@
+x=input("Please enter the first number")
+y=input("Please enter the Second number")
+
+x=float(x)
+y=float(y)
+
+sum=x+y
+difference=x-y
+product=x*y
+quotient=x/y
+remainder=x%y
+power=x**y
+
+
+print(f"The sum is {sum}  Difference is {difference} Product is {product} quotient is {quotient} remainder is {remainder} power is {power}")
+
+
+
---
+++ mathsfunctions.py
@@ -0,0 +1,12 @@
+from math import *
+#import math
+
+x= input("Please neter the number ")
+
+x=float(x)
+
+
+y=ceil(x)
+print(y)
+
+
